df3e04e9ab5d19dacf041788dde8f8cc886bf3ae
DavideCominelli <dvd.cominelli@gmail.com>
2026-02-13 16:46:34 +0100

    first iteration model Step1

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from gurobipy import GRB
 import pandas as pd
 import numpy as np 
 from OptimizationClasses import LP_InputData, Expando, LP_OptimizationProblem
-from data.data import unit_cost_G, technical_data_G, technical_data_W
+from data.data import *
 
 num_variables_G = 12
 num_variables_W = 6
@@ -24,7 +24,8 @@ L_KEYS = [f"l{i+1}" for i in range(num_constraints_L)]
 # objective coefficients
 objective_coeff = {f"g{i+1}": unit_cost_G["C_i"][i] for i in range(num_variables_G)}
 objective_coeff |= {f"w{i+1}": 0 for i in range(num_variables_W)}
-objective_coeff |= {f"d{i+1}": 0 for i in range(num_variables_D)}
+
+objective_coeff |= {f"d{i+1}": -demand['Bid_price'][i] for i in range(num_variables_D)}
 
 # helper to build a constraint coeff dict that contains all variables (zero for others)
 def one_hot_coeff_for_index(idx, sign=1):
@@ -43,20 +44,38 @@ for i in range(num_constraints_U):
 for i in range(num_constraints_L):
     constraints_coeff[L_KEYS[i]] = one_hot_coeff_for_index(i, sign=-1)
 
-# balance constraint: sum of all generation + wind == demand
-constraints_coeff["balance"] = {v: 1 for v in VARIABLES}
+constraints_coeff["balance"] = {v: 1 for v in VARIABLES[:len(VARIABLES)-num_variables_D]} 
+constraints_coeff["balance"].update({v: -1 for v in VARIABLES[len(VARIABLES)-num_variables_D:]}) # only generators and wind in balance constraint
 
 # build RHS: upper bounds from technical data, lower bounds zero, balance set to demand
-constraints_rhs = {}
-for i in range(num_variables_G):
-    constraints_rhs[U_KEYS[i]] = technical_data_G["P_max"][i]
-for i in range(num_variables_W):
-    constraints_rhs[U_KEYS[num_variables_G + i]] = technical_data_W["P_max"][i]
 
-for i in range(num_constraints_L):
-    constraints_rhs[L_KEYS[i]] = 0
+def rhs_function(num_variables_G, num_variables_W, num_variables_D):
+    constraints_rhs = {}
 
-constraints_rhs["balance"] = 2650.5
+    for i in range(num_variables_G):
+        constraints_rhs[U_KEYS[i]] = technical_data_G["P_max"][i]
+    for i in range(num_variables_W):
+        constraints_rhs[U_KEYS[num_variables_G + i]] = technical_data_W["P_max"][i]
+    for i in range(num_variables_D):
+        constraints_rhs[U_KEYS[num_variables_G + num_variables_W + i]] = demand['D_max %'][i]*max(load_curve['load_MW'])/100
+
+    constraints_rhs["balance"] = 0
+
+    for i in range(num_constraints_L):
+        constraints_rhs[L_KEYS[i]] = 0
+
+    return constraints_rhs
+
+#constraints_rhs = {}
+#for i in range(num_variables_G):
+#    constraints_rhs[U_KEYS[i]] = technical_data_G["P_max"][i]
+#for i in range(num_variables_W):
+#    constraints_rhs[U_KEYS[num_variables_G + i]] = technical_data_W["P_max"][i]
+#
+#for i in range(num_constraints_L):
+#    constraints_rhs[L_KEYS[i]] = 0
+
+#constraints_rhs["balance"] = 2650.5
 
 # constraint senses: use LESS_EQUAL for u and l (l uses -x <= 0), equality for balance
 constraints_sense = {k: GRB.LESS_EQUAL for k in U_KEYS + L_KEYS}
@@ -67,7 +86,7 @@ input_data = LP_InputData(
     CONSTRAINTS = U_KEYS + L_KEYS + ["balance"],
     objective_coeff = objective_coeff,
     constraints_coeff = constraints_coeff,
-    constraints_rhs = constraints_rhs,
+    constraints_rhs = rhs_function(num_variables_G, num_variables_W, num_variables_D),
     constraints_sense = constraints_sense,
     objective_sense = GRB.MINIMIZE,
     model_name = "Economic dispatch problem"
@@ -76,3 +95,5 @@ input_data = LP_InputData(
 problem = LP_OptimizationProblem(input_data)
 problem.run()
 problem.display_results()
+
+# hola
